aa4ab59116153e3dd1476ca83fc8a8fe8acfd9e4
ms-iwade <ms_iwade@trust-coms.com>
2025-12-01 18:52:31 +0900

    fix

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,33 +1,34 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { functionWithDataAccess } from "../function/functionWithDataAccess/resource";
 
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-      status: a.enum(["PENDING", "COMPLETED"]),
-      createdAt: a.datetime(),
-    })
-    .secondaryIndexes((index) => [
-      // ステータス別にTodoを作成日時順で検索するためのIndex
-      index("status").sortKeys(["createdAt"]).queryField("todosByStatus"),
-    ])
-    .authorization((allow) => [allow.authenticated()]),
+const schema = a
+  .schema({
+    Todo: a
+      .model({
+        content: a.string(),
+        status: a.enum(["PENDING", "COMPLETED"]),
+        createdAt: a.datetime(),
+      })
+      .secondaryIndexes((index) => [
+        index("status").sortKeys(["createdAt"]).queryField("todosByStatus"),
+      ])
+      .authorization((allow) => [allow.authenticated()]),
 
-  // カスタムミューテーション
-  createCustomTodo: a
-    .mutation()
-    .arguments({
-      content: a.string().required(),
-      status: a.enum(["PENDING", "COMPLETED"]),
-    })
-    .returns(a.json())
-    .authorization((allow) => [allow.authenticated()])
-    .handler(a.handler.function(functionWithDataAccess)),
-})
-.authorization(allow => [
-  allow.resource(functionWithDataAccess).to(["mutate"]) // 関数からデータにmutateできるようにするため、Schemaに対してMutateの許可を設定
-]);
+    createCustomTodo: a
+      .mutation()
+      .arguments({
+        content: a.string().required(),
+        status: a.enum(["PENDING", "COMPLETED"]),
+      })
+      .returns(a.json())
+      .authorization((allow) => [allow.authenticated()])
+      .handler(a.handler.function(functionWithDataAccess)),
+
+    // 上記はサンプルコードです。開発の際はここより下に追加してください。
+  })
+  .authorization((allow) => [
+    allow.resource(functionWithDataAccess).to(["mutate"]),
+  ]);
 
 export type Schema = ClientSchema<typeof schema>;
 
--- amplify/function/functionWithDataAccess/resource.ts
+++ amplify/function/functionWithDataAccess/resource.ts
@@ -1,11 +1,11 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { defineFunction } from "@aws-amplify/backend";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/functions/configure-functions/
  */
 
 export const functionWithDataAccess = defineFunction({
-  name: 'functionWithDataAccess',
-  resourceGroupName: 'data', // この関数は主にDynamoDBにアクセスするため、dataリソースと同じリソースグループに配置する
+  name: "functionWithDataAccess",
+  resourceGroupName: "data", // この関数は主にDynamoDBにアクセスするため、dataリソースと同じリソースグループに配置する
   runtime: 20, // useNodejs20.x
 });
